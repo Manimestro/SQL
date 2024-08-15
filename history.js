154610ae5606ef529ac4075240c4caa73f9b3cc3
manikanta <manikantatez@gmail.com>
2024-08-16 01:59:24 +0530

    Introduction

---
+++ history.js
@@ -0,0 +1,10 @@
+/*
+! Introduction
+The old manual data entry had some problems so introduced databases 
+
+! Nonrelational Database Systems
+early days databases are hyrancyal tress with parents ,nodes, and childrens
+
+
+
+*/
