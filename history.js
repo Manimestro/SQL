30e939ab4e7363ca6fe09751f07709703228c6c2
manikanta <manikantatez@gmail.com>
2024-08-17 20:19:19 +0200

    Relational Model,Sql

--- history.js
+++ history.js
@@ -5,6 +5,21 @@ The old manual data entry had some problems so introduced databases
 ! Nonrelational Database Systems
 early days databases are hyrancyal tress with parents ,nodes, and childrens
 
+! The Relational Model
+1970's 
+proposal was to keep tables to store data in a row for datasets and columns for data types 
+primary key --> defines a row uniqly 
+compund key --> if multiples colums are used to define a row 
+foreign key --> key to relate one table to another 
+
+No data redundancy 
+Normalisation techniques --> to keep data integrety well
+
+! SQL
+language to work with relational db's 
+DSL/Alpha --> Square --> Sql 
+The result of an SQL query is a table
+so we can  create  a record with just the result 
 
 
 */
